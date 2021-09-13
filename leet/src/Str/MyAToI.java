4c1c5d07114e17ee29642a72e844430c2b7ba67c
zhaojunjie <1278441739@qq.com>
2021-09-14 03:08:25 +0800

    变量修正

--- leet/src/Str/MyAToI.java
+++ leet/src/Str/MyAToI.java
@@ -3,7 +3,7 @@ package Str;
 public class MyAToI {
 
     public static void main(String[] args) {
-        System.out.println(myatoi(" -41"));
+        System.out.println(myatoi("words and 987"));
     }
 
     public static int myatoi(String s) {
@@ -31,7 +31,7 @@ public class MyAToI {
         int res = 0;
         while(index <length){
             int num = chs[index]-'0';
-            if(num < 0 || index > 9){
+            if(num < 0 || num > 9){
                 break;
             }
             tmp = res;
