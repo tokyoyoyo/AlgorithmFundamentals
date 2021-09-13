747b1883b2f8b4726ba9e4f580c9e3b00cdcddf0
zhaojunjie <1278441739@qq.com>
2021-09-14 02:54:10 +0800

    AtoI

---
+++ leet/src/Str/MyAToI.java
@@ -0,0 +1,51 @@
+package Str;
+
+public class MyAToI {
+
+    public static void main(String[] args) {
+        System.out.println(myatoi(" -41"));
+    }
+
+    public static int myatoi(String s) {
+        char[] chs = s.toCharArray();
+        int length = chs.length;
+        int index = 0;
+
+        while(index < length && chs[index] == ' '){
+            index++;
+        }
+
+        if (index >= length){
+            return 0;
+        }
+
+        int sign = 1;
+        if(chs[index] == '-' || chs[index] == '+'){
+            if(chs[index] == '-'){
+                sign = -1;
+            }
+            index++;
+        }
+
+        int tmp = 0;
+        int res = 0;
+        while(index <length){
+            int num = chs[index]-'0';
+            if(num < 0 || index > 9){
+                break;
+            }
+            tmp = res;
+            res = tmp*10 + num;
+            if(res/10 != tmp){
+                if(sign == 1){
+                    return Integer.MAX_VALUE;
+                }else {
+                    return Integer.MIN_VALUE;
+                }
+            }
+            index++;
+        }
+        return res*sign;
+
+    }
+}
